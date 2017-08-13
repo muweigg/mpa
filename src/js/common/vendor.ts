cecfaa3235ed87498a8f6d581082bb6ded773a74
Wei Mu <[muweigg@gmail.com]>
2017-08-13 11:22:25 +0800

    update

--- src/js/common/vendor.ts
+++ src/js/common/vendor.ts
@@ -1,5 +1,6 @@
 // import 'script-loader!babel-loader!./third-party/Rx';
 import 'script-loader!./third-party/Rx.min';
 import 'script-loader!lodash/lodash.min';
-import 'script-loader!jquery/dist/jquery.min';
+import 'script-loader!jquery/dist/jquery.slim.min';
 import 'script-loader!vue/dist/vue.min';
+import 'script-loader!axios/dist/axios.min';
