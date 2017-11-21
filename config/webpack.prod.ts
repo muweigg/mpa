8bf1e4af1b694598f6f34f86eccf64aaa4c474c8
[muweigg] <[muweigg@gmail.com]>
2017-11-21 14:45:46 +0800

    update

--- config/webpack.prod.ts
+++ config/webpack.prod.ts
@@ -47,9 +47,9 @@ export default webpackMerge(config({ env: ENV }), {
             sourceMap: false,
             parallel: true,
             uglifyOptions: {
-                mangle: { ie8: true },
-                compress: { ie8: true, warnings: false, drop_console: true },
+                compress: { warnings: false, drop_console: true },
                 output: { comments: false },
+                ie8: true,
             }
         }),
         // new ManifestPlugin(),
