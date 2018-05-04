4c011ecc12be5d0e68e9da29e96fa8aab09cb105
[muweigg] <muweigg@gmail.com>
2018-05-04 14:15:12 +0800

    update

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,5 @@
 declare function isIE(): number | undefined;
+declare var rxjs: any;
 
 interface Window {
     isIE(): number | undefined;
--- src/js/index.ts
+++ src/js/index.ts
@@ -1,3 +1,5 @@
 $( () => {
+    const { fromEvent } = rxjs;
+    fromEvent(document, 'click').subscribe(e => console.log(e));
     console.log('index.ts');
 } );
