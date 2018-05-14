c9ac66c391471ac723e0d9266b396bf6774c794d
[muweigg] <muweigg@gmail.com>
2018-05-14 17:59:42 +0800

    update

--- server/db.js
+++ server/db.js
@@ -11,7 +11,14 @@ module.exports = {
             "id|+1": 1,
             "author": "@name",
             "comment": "@cparagraph",
-            "date": "@datetime"
+            'date|+1' () {
+                let now = new Date('2018-01-00');
+                now.setTime(now.getTime() - (-this.id) * 24 * 60 * 60 * 1000);
+                const year = now.getFullYear();
+                const month = now.getMonth() + 1;
+                const day = now.getDate();
+                return `${year<10?0:''}${year}-${month<10?0:''}${month}-${day<10?0:''}${day}`;
+            }
         }]
     })
 }
